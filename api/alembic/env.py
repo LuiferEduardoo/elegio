312ba952831dcab33fadea48d4bf309ad49351c9
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-04-30 21:53:03 -0500

    feat(api): add migration

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -16,6 +16,7 @@ from app.domains.category import models as _category_models  # noqa: F401
 from app.domains.government_plan import models as _government_plan_models  # noqa: F401
 from app.domains.proposal import models as _proposal_models  # noqa: F401
 from app.domains.source import models as _source_models  # noqa: F401
+from app.domains.tagging import models as _tagging_models  # noqa: F401
 
 config = context.config
 
