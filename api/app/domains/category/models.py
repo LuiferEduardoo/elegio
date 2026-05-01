8880f92737dde91dbfe9446ff1d64c31d1f19dff
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-04-30 21:36:54 -0500

    feat(api): add field weight

--- api/app/domains/category/models.py
+++ api/app/domains/category/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import Float, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base, TimestampMixin
@@ -9,3 +9,4 @@ class Category(Base, TimestampMixin):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
+    weight: Mapped[float] = mapped_column(Float, nullable=False)
