83c5f7521f1ae25c62bc5fb35956f8a5e12abb2d
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-05-01 18:56:06 -0500

    feat(api): delete router visitor

--- api/app/api/v1/router.py
+++ api/app/api/v1/router.py
@@ -9,10 +9,8 @@ from app.domains.question.routes import router as question_router
 from app.domains.response_option.routes import router as response_option_router
 from app.domains.test.routes import router as test_router
 from app.domains.test_attempt.routes import router as test_attempt_router
-from app.domains.visitor.routes import router as visitor_router
 
 api_router = APIRouter(prefix="/api/v1")
-api_router.include_router(visitor_router)
 api_router.include_router(test_router)
 api_router.include_router(test_attempt_router)
 api_router.include_router(candidate_router)
