cc62fc9f29d48f0bbdb5670bf05862556b9945d8
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-05-15 22:04:56 -0500

    feat(analysis): tests

---
+++ analysis/sql/tests.sql
@@ -0,0 +1,8 @@
+INSERT INTO tests (
+    name,
+    description,
+    created_at,
+    updated_at,
+    deleted_at
+) VALUES
+('¿Quién es tu candidato ideal?', 'Responde estas preguntas y descubre qué candidato se alinea mejor con tus ideas y valores.', NOW(), NOW(), NULL);
